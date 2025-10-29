de43cf2684fa819dc39f7f1860b899403e2bf205
Aldo Aldiero <rivelinoaldo@gmail.com>
2025-10-29 19:01:52 +0700

    fix: lint

--- src/pages/dashboard/ApplyJobOpening.tsx
+++ src/pages/dashboard/ApplyJobOpening.tsx
@@ -3,7 +3,7 @@ import CreateApplicationForm from "@/components/application/create/create-applic
 export default function ApplyJobOpeningPage() {
   return (
     <div className="flex flex-col w-full h-full">
-      <CreateApplicationForm/>
+      <CreateApplicationForm />
     </div>
   );
 }
